04cabe2cc1bc4e5bbe28c8e69da2966d284967c8
bsalbs <bishal.bista@tekkon.com.np>
2024-11-19 23:28:05 +0545

    feat: update create page for bank and qr deposit

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -9,6 +9,7 @@ const CreateDataPage = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [bankDeposit, setBankDeposit] = useState("0");
+  const [qrDeposit, setQRdeposit] = useState("0");
 
   const initialPrices = [
     { amount: 5, quantity: "0", total: 0 },
@@ -42,8 +43,7 @@ const CreateDataPage = () => {
   };
 
   const calculateOverallTotal = () => {
-    const priceTotal = prices.reduce((acc, price) => acc + price.total, 0);
-    return priceTotal + (parseInt(bankDeposit, 10) || 0);
+    return prices.reduce((acc, price) => acc + price.total, 0);
   };
 
   const handleEntry = async () => {
@@ -56,6 +56,7 @@ const CreateDataPage = () => {
           quantity: parseInt(price.quantity, 10),
         })),
         bankDeposit: parseInt(bankDeposit, 10),
+        qrDeposit: parseInt(qrDeposit, 10),
         total: calculateOverallTotal(),
       };
       await saveData(payload);
@@ -77,7 +78,7 @@ const CreateDataPage = () => {
         Back
       </span>
       <h2 className="text-xl font-semibold mb-4 mt-10">Create Entry</h2>
-      <div className="w-1/2">
+      <div className="w-full">
         <div className="mb-10">
           <label htmlFor="date" className="mr-4">
             Select Date:
@@ -91,54 +92,77 @@ const CreateDataPage = () => {
           />
         </div>
 
-        <table className="w-full table-auto text-md border-collapse border border-gray-300">
-          <thead className="text-left bg-gray-200">
-            <tr>
-              <th className="border px-4 py-2">SN</th>
-              <th className="border px-4 py-2">Amount</th>
-              <th className="border px-4 py-2">Quantity</th>
-              <th className="border px-4 py-2">Total</th>
-            </tr>
-          </thead>
-          <tbody className="bg-gray-100">
-            {prices.map((price, index) => (
-              <tr key={index}>
-                <td className="border px-4 py-2">{index + 1}</td>
-                <td className="border px-4 py-2">{price.amount}</td>
+        <div className="flex flex-col gap-5">
+          <table className="w-full table-auto text-md border-collapse border border-gray-300">
+            <thead className="text-left bg-gray-200">
+              <tr>
+                <th className="border px-4 py-2">SN</th>
+                <th className="border px-4 py-2">Amount</th>
+                <th className="border px-4 py-2">Quantity</th>
+                <th className="border px-4 py-2">Total</th>
+              </tr>
+            </thead>
+            <tbody className="bg-gray-100">
+              {prices.map((price, index) => (
+                <tr key={index}>
+                  <td className="border px-4 py-2">{index + 1}</td>
+                  <td className="border px-4 py-2">{price.amount}</td>
+                  <td className="border px-4 py-2">
+                    <CustomInput
+                      value={price.quantity}
+                      onChange={(value) => handleQuantityChange(index, value)}
+                      className="border px-2 py-1 w-24 text-black"
+                    />
+                  </td>
+                  <td className="border px-4 py-2">{price.total}</td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr className="bg-gray-200">
+                <td
+                  colSpan={3}
+                  className="border px-4 py-2 font-bold text-right"
+                >
+                  Overall Total
+                </td>
+                <td className="border px-4 py-2 font-bold text-right">
+                  {calculateOverallTotal()}
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+          <table className="w-full table-auto text-md border-collapse border rounded-lg">
+            <thead className="text-left bg-gray-200">
+              <tr>
+                <th className="border px-4 py-2">SN</th>
+                <th className="border px-4 py-2">Bank Deposit</th>
+                <th className="border px-4 py-2">QR/Fonepay</th>
+              </tr>
+            </thead>
+            <tbody className="bg-gray-100">
+              <tr>
+                <td className="border px-4 py-2">1</td>
                 <td className="border px-4 py-2">
+                  {" "}
                   <CustomInput
-                    value={price.quantity}
-                    onChange={(value) => handleQuantityChange(index, value)}
+                    value={bankDeposit}
+                    onChange={setBankDeposit}
                     className="border px-2 py-1 w-24 text-black"
                   />
                 </td>
-                <td className="border px-4 py-2">{price.total}</td>
+                <td className="border px-4 py-2">
+                  {" "}
+                  <CustomInput
+                    value={qrDeposit}
+                    onChange={setQRdeposit}
+                    className="border px-2 py-1 w-24 text-black"
+                  />
+                </td>
               </tr>
-            ))}
-          </tbody>
-          <tfoot>
-            <tr className="bg-gray-100">
-              <td colSpan={3} className="border px-4 py-2 font-bold text-right">
-                Bank Deposit
-              </td>
-              <td className="border px-4 py-2 font-bold text-right">
-                <CustomInput
-                  value={bankDeposit}
-                  onChange={setBankDeposit}
-                  className="border px-2 py-1 w-24 text-black"
-                />
-              </td>
-            </tr>
-            <tr className="bg-gray-200">
-              <td colSpan={3} className="border px-4 py-2 font-bold text-right">
-                Overall Total
-              </td>
-              <td className="border px-4 py-2 font-bold text-right">
-                {calculateOverallTotal()}
-              </td>
-            </tr>
-          </tfoot>
-        </table>
+            </tbody>
+          </table>
+        </div>
         <div className="flex justify-end mt-5">
           <button
             className="px-4 py-2 rounded-md border border-black hover:border-none hover:bg-purple-400 hover:text-white font-bold transition-all"
